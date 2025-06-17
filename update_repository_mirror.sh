eeb07acc57f8ff5bf97137fa384c0f10c877d5b4
Raphael Von Moos <raphael.vonmoos@kauz.ch>
2025-06-17 11:06:41 +0200

    feat: add condition if cd fails

--- update_repository_mirror.sh
+++ update_repository_mirror.sh
@@ -1,9 +1,16 @@
 . .config
 
-cd "$absolute_path_to_cloned_repo"
+# Make sure that the cloned repository (still) exists
+git ls-remote "$absolute_path_to_cloned_repo" -q
 
-# Download objects and refs from origin repository, prune unreachable objects
-git fetch -p origin
+if [ $? -eq 0 ]; then
+    cd "$absolute_path_to_cloned_repo"
 
-# Update remote refs along with associated objects to target repo
-git push --mirror
+    # Download objects and refs from origin repository, prune unreachable objects
+    git fetch -p origin
+
+    # Update remote refs along with associated objects to target repo
+    git push --mirror
+else
+    echo "The local repository folder has been deleted or has been moved."
+fi
